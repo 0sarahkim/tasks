714bc6a5a001283412493c2f77212b5992522318
SarahK <sk4951@columbia.edu>
2022-03-22 16:36:43 -0400

    fixed bugs

--- tasks.py
+++ tasks.py
@@ -11,54 +11,72 @@ import csv
 import os
 
 
-DONE = " ✅"
+DONE = " V"
 
 
 def list():
     """
     List the current known tasks.
     """
-
-    with open("tasks.csv") as tasks_file:
+    with open("tasks.csv", newline= '') as tasks_file:
         reader = csv.reader(tasks_file)
-        for name, completed in reader:
-            print(f"{name}{DONE if completed else ''}")
-
+        for i in reader:
+            if len(i) == 2:
+                name, completed = i
+                if not completed == 'False':
+                    print(f"{name}{DONE}")
+                else:
+                    print(f"{name}{''}")
+    
 
 def create(name):
     """
     Create a new task.
     """
+    rows = []
+    with open("tasks.csv", "r") as tasks_file:
+        for row in csv.reader(tasks_file):
+            if len(row) > 0:  
+                rows.append(row[0])
 
     with open("tasks.csv", "a") as tasks_file:
         writer = csv.writer(tasks_file)
-        writer.writerow([name, False])
+        if name not in rows:
+            writer.writerow([name, False])
+        else:
+           print(f"{name}{' already exists in the list'}")
 
 
 def complete():
     """
     Mark an existing task as completed.
     """
-
     with (
         open("tasks.csv") as tasks_file,
         NamedTemporaryFile("w", delete=False) as new,
     ):
         reader = csv.reader(tasks_file)
         print("Current tasks:")
-        for id, (name, completed) in enumerate(reader):
-            print(id, name, completed)
+        for i in enumerate(reader,1):
+            if len(i) == 3:
+                print(id, name, completed)
 
         to_complete = int(input("task ID?> "))
         writer = csv.writer(new)
+        print(writer)
         tasks_file.seek(0)
-        for id, (name, completed) in enumerate(reader):
-            if id == to_complete:
-                writer.writerow([name, True])
-            else:
-                writer.writerow([name, completed])
 
-    os.rename(new.name, "tasks.csv")
+        for i in enumerate(reader,1):
+            if len(i[1]) == 2:
+                print(i)
+                id, name, completed = i[0], i[1][0], i[1][1]
+                if id == to_complete:
+                    writer.writerow([name, True])
+                else:
+                    print(writer.writerow([name, completed]))
+
+
+    os.rename(new.name ,"tasks.csv")
 
 
 operations = dict(
@@ -76,9 +94,11 @@ def main():
             return
 
         operation, *args = line.split()
+                
         fn = operations.get(operation)
         fn(*args)
 
-
 if __name__ == "__main__":
     main()
+
+
